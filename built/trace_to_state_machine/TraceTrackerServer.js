49e8647220c59fd73ad23e06b41ab46665304e0d
Steve Oney <swloney@gmail.com>
2018-08-26 22:45:26 -0400

    abc

--- built/trace_to_state_machine/TraceTrackerServer.js
+++ built/trace_to_state_machine/TraceTrackerServer.js
@@ -3,11 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sdb_ts_1 = require("sdb-ts");
 const http = require("http");
 const WebSocket = require("ws");
+const express = require("express");
+const path = require("path");
 const PORT = 8000;
-const server = http.createServer();
+const app = express();
+app.use(express.static(path.join(__dirname, 'client_pages')));
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const sdbServer = new sdb_ts_1.SDBServer(wss);
 const userTracesDoc = sdbServer.get('t2sm', 'userTraces');
 userTracesDoc.createIfEmpty({});
 server.listen(PORT);
+console.log(`Listening on port ${PORT}`);
 //# sourceMappingURL=TraceTrackerServer.js.map
